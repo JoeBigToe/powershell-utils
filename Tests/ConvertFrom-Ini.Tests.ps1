5b3788a2ee0177f600ee43d2fe6413be1da73ab4
JoeBigToe <jrosa.main@gmail.com>
2020-03-30 19:31:59 +0200

    Commited first pester test

--- Tests/ConvertFrom-Ini.Tests.ps1
+++ Tests/ConvertFrom-Ini.Tests.ps1
@@ -6,18 +6,31 @@ $modulePath = $MyInvocation.MyCommand.Path -replace '\\Tests\\.*', "\$moduleName
 $sutName = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 $sutPath = gci -recurse -file -Path $modulePath -Filter $sutName | select -expand "fullname"
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$global:test_data = "$here\data"
 
 Import-Module -Force $modulePath
 
 InModuleScope $moduleName {
-    Describe "ConvertFrom-Ini" {
 
-        it "Should return a hashtable" {
+    context "When input message is well formatted" {
 
-            ConvertFrom-Ini "test" | Should -BeOfType [hashtable]
+        $message = Get-Content "$global:test_data\well-formatted.ini"
 
-        } 
+        Describe "ConvertFrom-Ini" {
 
+            $output = ConvertFrom-Ini $message
+
+            it "Should return a hashtable" {
+
+                $output | Should -BeOfType [hashtable]
+
+            }
+            
+            it "Should return chapters and key values" {
+                $output.chapter1.key1 | Should -Be "val1"
+                $output.chapter2.key1 | Should -Be "val3"
+            }
+
+        }
     }
-
 }
